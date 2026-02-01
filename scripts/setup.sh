8c1fe5b35a9a033339c2e3a237af166c436233dc
Kunzhao Ren <rkzdtc@gmail.com>
2026-02-01 16:36:10 -0600

    fix: correct variable assignment for project directory in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,13 +12,14 @@ ROS_DISTRO="${2}"
 echo "Setup unitree ros2 environment (interface: ${IFACE})"
 
 # Get project root directory
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/../../../
+WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/../../../
+echo "Set WORKSPACE: $WORKSPACE"
 
-export WORKSPACE=$PROJECT_DIR
+export WORKSPACE=$WORKSPACE
 export NetworkInterface=${IFACE}
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
-source install/setup.bash
+source $WORKSPACE/install/setup.bash
 
 sudo ip link set dev lo multicast on
 
